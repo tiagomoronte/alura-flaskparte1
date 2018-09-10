6cd9683b2f7c1590f892c5df47033aed3bb2be9f
Tiago Moronte <tiagomoronte@mpf.mp.br>
2018-09-09 22:12:19 -0300

    Aula 2

--- jogoteca/jogoteca.py
+++ jogoteca/jogoteca.py
@@ -2,9 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
 @app.route('/inicio')
 def ola():
-    return render_template("lista.html");
+    lista = [Jogo("Super Mario", "Ação", "SNES"), Jogo("Pokemon Gold", "RPG", "GBA")]
+    return render_template("lista.html", titulo='Jogos', jogos=lista)
 
 
 app.run()
